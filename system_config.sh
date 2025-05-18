3904f604582c1d62eb96eb003cb63f267ea80047
Jayalekshmi M <ammusz749@gmail.com>
2025-05-18 10:04:16 +0530

    pgm 14

---
+++ system_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Current shell: $SHELL"
+echo "Home directory: $HOME"
+echo "Operating System type: $OSTYPE"
+echo "Current working directory: "
+pwd
+
+# to show various system configuration(pgm 14)
